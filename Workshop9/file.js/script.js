c8d1d022e334067543acaa0d386686b9c9dd89e4
noilchitrakar <np03cs4a220175@heraldcollege.edu.np>
2023-01-21 12:10:18 +0545

    portfolio website

---
+++ Workshop9/file.js/script.js
@@ -0,0 +1,18 @@
+console.log("hello world");
+document.getElementById("paragraph").innerHTML="<h1>hello world</h1>";
+// document.getElementById("paragraph").style.color="black";
+// document.getElementById("paragraph").style.backgroundColor="green";
+document.getElementById("paragraph").style.cssText="color:aqua; background:grey"
+
+var age=24;
+age=13;
+const age1=24;
+age1=2;
+let age2=54;
+age2=8;
+var name="jenny";
+
+console.log(age);
+console.log(age1);
+console.log(age2);
+console.log(name);
